9bba10bef0887be5f9c61c5c7774651eef2f9da5
Esther <150644846+ebrandwein21@users.noreply.github.com>
2025-05-09 00:22:30 -0500

    First Push, had to start new project

---
+++ recipeEncyclopedia/recipeEncyclopedia/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace recipeEncyclopedia
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ recipeEncyclopedia/recipeEncyclopedia/Models.cs/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recipeEncyclopedia.Models.cs
+{
+    class Recipe
+    {
+    }
+}
