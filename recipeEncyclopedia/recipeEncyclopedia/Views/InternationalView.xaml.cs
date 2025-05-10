9315d8ec0875876e243cdaea3ac77dce6d18eb82
Esther <150644846+ebrandwein21@users.noreply.github.com>
2025-05-09 20:54:25 -0500

    added more button clicks and recipe pages
    added more button clicks and recipe pages

---
+++ recipeEncyclopedia/recipeEncyclopedia/Views/InternationalView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace recipeEncyclopedia.Views
+{
+    /// <summary>
+    /// Interaction logic for InternationalView.xaml
+    /// </summary>
+    public partial class InternationalView : Window
+    {
+        public InternationalView()
+        {
+            InitializeComponent();
+        }
+    }
+}
